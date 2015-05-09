e99d8a6624a13e76346b9d030d663d5422124ac5
rogerbutt <andrew.luu.93@gmail.com>
2015-05-09 18:54:16 -0500

    such s3's

--- client/app/uploadNote/uploadNote.controller.js
+++ client/app/uploadNote/uploadNote.controller.js
@@ -10,12 +10,23 @@ angular.module('notrApp')
     $scope.upload = function (files) {
         if (files && files.length) {
             for (var i = 0; i < files.length; i++) {
-                //var file = files[i];
+                var file = files[i];
+                console.log(file);
 
                 // File Upload for S3, but have to be careful
 
                 /* Upload.upload({
-                    url: 'upload/url',
+                    url: 'https://cs498rk-notr.s3.amazonaws.com/',
+                    method: 'POST',
+                    fields : {
+                      key: file.name,
+                      AWSAccessKeyId: <YOUR AWS AccessKey Id>, 
+                      acl: 'public',
+                      policy: $scope.policy,
+                      signature: $scope.signature,
+                      "Content-Type": file.type != '' ? file.type : 'application/octet-stream',
+                      filename: file.name
+                    },
                     file: file
                 }).progress(function (evt) {
                     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
