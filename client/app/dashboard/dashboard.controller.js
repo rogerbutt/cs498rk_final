1eb2db9b4ec037e4a15ccae65fff632f7d24f4c8
harjas <harjassinghglobal@gmail.com>
2015-05-10 01:48:07 -0500

    design changes on search and dash pages

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -5,5 +5,28 @@ angular.module('notrApp')
     $scope.message = 'Hello';
     notesService.getNotes(function (notes) {
         $scope.notes = notes;
+        // $scope.dates = [];
+
+        // for(var i=0; i<notes.length; i++){
+        // 	$scope.dates[i] = dateFormat(notes[i].date);
+        // }
+
+        $scope.dateFormat = function(date){
+	    	console.log("getting date");
+	    	var monthNames = [
+		        "January", "February", "March",
+		        "April", "May", "June", "July",
+		        "August", "September", "October",
+		        "November", "December"
+		    ];
+		    var ret = "";
+		    var date = new Date(date);
+		    var day = date.getDate();
+		    var monthIndex = date.getMonth();
+		    var year = date.getFullYear();
+
+		    ret+= monthNames[monthIndex]+" "+day+", " + year;
+	  		return ret;
+    	}
     });
   }]);
