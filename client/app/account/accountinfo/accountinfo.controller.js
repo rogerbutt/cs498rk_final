72e7d5abe93fdb536feb7166d353c8d9431e1a6a
Annie Rong <annie@Annies-MacBook-Pro.local>
2015-05-09 19:47:13 -0500

    fixed credits

--- client/app/account/accountinfo/accountinfo.controller.js
+++ client/app/account/accountinfo/accountinfo.controller.js
@@ -3,4 +3,15 @@
 angular.module('notrApp')
   .controller('AccountInfoCtrl', function ($scope, User) {
     $scope.user = User.get();
+    $scope.additionalCredits;
+
+    $scope.credit = function(form) {
+      $scope.user.credits = $scope.user.credits + $scope.additionalCredits;
+      $scope.additionalCredits = 0;
+      User.addCredit({id:$scope.user._id}, $scope.user);
+	};  
+
+	$scope.deleteCard = function() {
+		
+	}
   });
