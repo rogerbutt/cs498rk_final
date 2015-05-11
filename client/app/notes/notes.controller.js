f064b3f51f89b1d6a6d7083b2ca2e10ada95e784
Sri Vasamsetti <sri.vasamsetti22@gmail.com>
2015-05-10 21:05:55 -0500

    sri messed up

--- client/app/notes/notes.controller.js
+++ client/app/notes/notes.controller.js
@@ -17,25 +17,15 @@ angular.module('notrApp')
     $scope.submitComment = function() {
     	$scope.submittedComment = true;
 
-<<<<<<< HEAD
-=======
-        // console.log($routeParams);
-        // console.log($scope.comment);
->>>>>>> 07d79acb8d3a666f29d2aa50b7ff6bda256c41a3
     	if (typeof $scope.comment !== 'undefined') {
     		$scope.comment.user = currentUser._id;    		
             $scope.comment.noteRef = $scope.note._id;
             console.log($scope.comment);
-            
-<<<<<<< HEAD
-            commentsService.postComment ($scope.comment, function (returnMessage ) {
-=======
 
             commentsService.postComment ($scope.comment, function () {
->>>>>>> 07d79acb8d3a666f29d2aa50b7ff6bda256c41a3
                 commentsService.getComments({ id: $routeParams.id }, function (comments) {
-                $scope.comments = comments;
-                $scope.comment = null;
+                    $scope.comments = comments;
+                    $scope.comment = null;
                 });
             });  
     	}
@@ -46,42 +36,17 @@ angular.module('notrApp')
             $scope.comment.rating = rating + 1;
         }   
     };
-    // $scope.setRating = function (rating) {
-    //     $scope.comment.rating = rating + 1;
-    // };
 
-<<<<<<< HEAD
-    // $scope.number = 5;
     $scope.getNumber = function(num) {
         // console.log(num);
         return new Array(num);   
     };
 
-    $scope.updateDate = function(date) {
-        return  date.val();
-        // var $scope.date = "";
-        // $scope.date = date.val().split('-');
 
-
-        //     date = $date.val().split('-'),
-        //     format = ['year', 'month', 'day'];
-        // $.each(format, function(i, v) {
-        //     $date.attr('data-' + v, +date[i]);
-        // });
-
-        // 2015-05-10T02:39:08.938Z
-    };
-
-
-
-
-=======
-    };
 
     $scope.setRating = function (rating) {
         $scope.comment.rating = rating + 1;
     };
->>>>>>> 07d79acb8d3a666f29d2aa50b7ff6bda256c41a3
 
   }]);
 
